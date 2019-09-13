49dcec556990055a2a019e9b8f6f48ef7b558c1d
Ethan Tom <ethan@planet.com>
2019-09-13 15:54:26 -0700

    added edit/delete plus more

--- controllers/dashboard.py
+++ controllers/dashboard.py
@@ -53,6 +53,20 @@ def checkTimeInAttendance(student_id):
     return False
 
 
+# this function creates sql statement with values only if variables are not null
+def editSQLStatement(data):
+    sql = ""
+    val = ""
+    for key, value in data.items():
+        if(value != None):
+            sql = sql + "`" + key + "` = %s,"
+            val = val + value + ","
+    sql = sql[:-1]
+    val = val[:-1]
+    return sql, val
+
+# --------------------------------- Parent Functions ----------------------------------------
+
 @app.route('/parent/all', methods=['GET'])
 def getAllParents():
     mydb = connect()
@@ -62,7 +76,7 @@ def getAllParents():
     return jsonify(parents), 200
 
 
-@app.route('/parent', methods=['POST'])
+@app.route('/parent/one', methods=['POST'])
 def getParent():
     data = {
         "first_name": request.form['first_name']
@@ -78,28 +92,6 @@ def getParent():
             return jsonify(parent), 200
     return "Parent not found", 404
 
-
-@app.route('/student/all', methods=['GET'])
-def selectAllStudents():
-    mydb = connect()
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM students")
-    students = jsonify(mycursor.fetchall())
-    return students
-
-
-@app.route('/student', methods=['POST'])
-def getStudent():
-    mydb = connect()
-    data = {
-        "qrcode": request.form['qrcode']
-    }
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM students WHERE qrcode='" + data["qrcode"] + "'")
-    student = mycursor.fetchone()
-    if(student[10]==data["qrcode"]):
-        return jsonify(student), 200
-    return "User not found", 404
 
 
 @app.route('/parent/add', methods=['POST'])
@@ -127,12 +119,12 @@ def addParent():
     except mysql.connector.Error as error:
         mydb.rollback()
         print("Failed inserting record into parent table: {}".format(error))
-        return "error"
-    return "success"
+    return "error"
 
 
 @app.route('/parent/edit', methods=['PUT'])
-def addParent():
+def editParent():
+    id = request.form["id"]
     data = {
         "first": request.form["first"],
         "middle": request.form["middle"],
@@ -145,19 +137,37 @@ def addParent():
         "guardian": request.form["guardian"],
         "notes": request.form["notes"]
     }
+    columns, val = editSQLStatement(data)
     mydb = connect()
     try:
         mycursor = mydb.cursor()
-        sql = "INSERT INTO `mydb`.`parents` (`first_name`, `middle_name`, `last_name`, `carrier`, `phone_number`, `email`, `messaging`, `emailing`, `guardian`, `notes`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
-        val = (data['first'], data['middle'], data['last'], data['carrier'], data['phone_number'], data['email'], data['messaging'], data['emailing'], data['guardian'], data['notes'])
+        sql = "UPDATE students SET " + columns + " WHERE id = " + id +";"
         mycursor.execute(sql, val)
-        print("Parent Table: successfully inserted", data['first'], data['last'])
+        print("Parent Table: successfully updated", data['first'], data['last'])
         return mycursor.lastrowid
     except mysql.connector.Error as error:
         mydb.rollback()
-        print("Failed inserting record into parent table: {}".format(error))
-        return "error"
-    return "success"
+        print("Failed updating record in parent table: {}".format(error))
+    return "error"
+
+
+@app.route('/parent/delete', methods=['DELETE'])
+def deleteParent():
+    data = {
+        "parent_id" : request.form["parent_id"]
+    }
+    mydb = connect()
+    try:
+        mycursor = mydb.cursor()
+        sql = "DELETE FROM parents WHERE `parent_id` = %s;"
+        val = data["parent_id"]
+        mycursor.execute(sql, val)
+        print("Successfully deleted parent record: %s", data["parent_id"])
+        return "success"
+    except mysql.connector.Error as error:
+        mydb.rollback()
+        print("Failed to delete record from parent table: {}".format(error))
+    return "error"
 
 
 @app.route('/language/add', methods=['POST'])
@@ -178,6 +188,51 @@ def addLanguage():
         print("Failed inserting record into parent table: {}".format(error))
         return "error"
     return "success"
+
+
+@app.route('/language/delete', methods=['DELETE'])
+def deleteLanguage():
+    data = {
+        "parent_id": request.form["parent_id"],
+        "language": request.form["language"]
+    }
+    mydb = connect()
+    try:
+        mycursor = mydb.cursor()
+        sql = "DELETE FROM language WHERE `parent_id` = %s, `language` = %s;"
+        val = (data["parent_id"], data["language"])
+        mycursor.execute(sql, val)
+        print("Successfully deleted language record: %s, %s", data["parent_id"], data["language"])
+        return "success"
+    except mysql.connector.Error as error:
+        mydb.rollback()
+        print("Failed to delete record from parent table: {}".format(error))
+    return "error"
+
+
+# --------------------------------- Student Functions ----------------------------------------
+
+@app.route('/student/all', methods=['GET'])
+def getAllStudents():
+    mydb = connect()
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM students")
+    students = jsonify(mycursor.fetchall())
+    return students
+
+
+@app.route('/student', methods=['POST'])
+def getStudent():
+    mydb = connect()
+    data = {
+        "qrcode": request.form['qrcode']
+    }
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM students WHERE qrcode='" + data["qrcode"] + "'")
+    student = mycursor.fetchone()
+    if(student[10]==data["qrcode"]):
+        return jsonify(student), 200
+    return "User not found", 404
 
 
 @app.route('/student/add', methods=['POST'])
@@ -202,11 +257,59 @@ def addStudent():
         mycursor.execute(sql, val)
         print("Student, %s %s, successfully inserted", data["first"], data["last"])
         mydb.commit()
+        return "success"
     except mysql.connector.Error as error:
         mydb.rollback()
         print("Failed inserting record into student table: {}".format(error))
-        return "error"
-    return "success"
+    return "error"
+
+
+@app.route('/student/edit', methods=['PUT'])
+def editStudent():
+    student_id = request.form["student_id"]
+    data = {
+        "parent_1_id": request.form["parent_1_id"],
+        "parent_2_id": request.form["parent_2_id"],
+        "first": request.form["first"],
+        "middle": request.form["middle"],
+        "last": request.form["last"],
+        "math": request.form["math"],
+        "reading": request.form["reading"],
+        "notes": request.form["notes"],
+        "qrcode": request.form["qrcode"]
+    }
+    columns, val = editSQLStatement(data)
+    mydb = connect()
+    try:
+        mycursor = mydb.cursor()
+        sql = "UPDATE students SET "+ columns +" WHERE student_id = " + student_id + ";"
+        mycursor.execute(sql, val)
+        print("Student, %s %s %s, successfully edited", data["first"], data["last"], student_id)
+        mydb.commit()
+        return "success"
+    except mysql.connector.Error as error:
+        mydb.rollback()
+        print("Failed editing record in student table: {}".format(error))
+    return "error"
+
+
+@app.route('/student/delete', methods=['DELETE'])
+def deleteStudent():
+    data = {
+        "student_id" : request.form["student_id"]
+    }
+    mydb = connect()
+    try:
+        mycursor = mydb.cursor()
+        sql = "DELETE FROM students WHERE `student_id` = %s;"
+        val = data["student_id"]
+        mycursor.execute(sql, val)
+        print("Successfully deleted student record: %s", data["student_id"])
+        return "success"
+    except mysql.connector.Error as error:
+        mydb.rollback()
+        print("Failed to delete record from student table: {}".format(error))
+    return "error"
 
 
 @app.route('/timeInOut', methods=['POST'])
