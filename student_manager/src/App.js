269f3223560ce7984a808396cd3b0c0d9cd17e29
Ethan Tom <etom579@gmail.com>
2020-01-18 03:05:31 -0800

    refactoring to react + express + electron app

---
+++ student_manager/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import StudentTable from './pages/StudentTable';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <StudentTable/>
+    </>
+  );
+}
+
+export default App;
---
+++ student_manager/src/pages/StudentTable.js
@@ -0,0 +1,102 @@
+import React, { forwardRef } from 'react';
+import MaterialTable from 'material-table';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import Button from '@material-ui/core/Button';
+
+
+export default function StudentTable() {
+  
+  const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  };
+  
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'Student ID', field: 'student_id' },
+      { title: 'First', field: 'first_name' },
+      { title: 'Middle', field: 'middle_name' },
+      { title: 'Last Name', field: 'last_name' },
+      { title: 'Math', field: 'math' },
+      { title: 'Reading', field: 'reading' },
+      { title: 'Attendance', field: 'attendance' }
+    ],
+    data: [
+      { student_id: 1, first_name: 'Jonah', middle_name: 'Steven', last_name: 'Lee', math: 'true', reading: 'false', attendance: 'here', checkIn: true }
+    ],
+  });
+  
+  // eslint-disable-next-line no-unused-vars
+  function onRowDelete(oldData) {
+    new Promise( resolve => {
+      setTimeout( () => {
+        resolve();
+        setState( prevState => {
+          const data = [...prevState.data];
+          data.splice( data.indexOf( oldData ), 1 );
+          return { ...prevState, data };
+        } );
+      }, 600 );
+    } )
+  }
+  
+  const components = {
+    Action: props => (
+      <Button aria-label="checkIn" size="small"
+        disabled={!props.data.checkIn}
+        onClick={(event) => props.action.onClick(event, props.data)}
+      >
+      </Button>
+    )
+    
+  };
+  
+  return (
+    <MaterialTable
+      icons={tableIcons}
+      title="Students"
+      columns={state.columns}
+      data={state.data}
+      options={{actionsColumnIndex: -1}}
+      actions={[
+        {
+          icon: '',
+          tooltip: 'Save User',
+          onClick: (event, rowData) => alert("You saved " + rowData.name)
+        },
+        {
+          icon: 'delete',
+          tooltip: 'Delete User',
+          onClick: (event, rowData) => confirm("You want to delete " + rowData.name)
+        }
+      ]}
+    />
+  );
+}
